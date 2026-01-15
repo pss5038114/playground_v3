5db6ea86c6e73b8fd1f9529b51489865757fd4ae
pss5038114 <pss5038114@gmail.com>
2026-01-15 20:20:11 +0900

    Initial commit

--- schema.sql
+++ schema.sql
@@ -4,3 +4,13 @@ CREATE TABLE IF NOT EXISTS users (
     password_hash TEXT,
     nickname TEXT
 );
+
+CREATE TABLE IF NOT EXISTS user_decks (
+    user_id INTEGER PRIMARY KEY,
+    slot_0 TEXT,
+    slot_1 TEXT,
+    slot_2 TEXT,
+    slot_3 TEXT,
+    slot_4 TEXT,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
